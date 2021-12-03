d3e6fbdddbdf81c728b2d73ed570984ae386172e
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2021-12-03 11:14:35 +0530

    Delete notesapp directory

--- notesapp/serializers.py
+++
@@ -1,7 +0,0 @@
-# API Serializer
-from rest_framework.serializers import ModelSerializer
-from notesapp.models import NotesModel
-class NotesSerializer(ModelSerializer):
-    class Meta:
-        model=NotesModel
-        fields='__all__'
